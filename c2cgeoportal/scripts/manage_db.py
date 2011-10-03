b26505ee5b501391596dcc90168a062ad612185a
Bruno Binet <bruno.binet@camptocamp.com>
2011-10-03 15:43:16 +0000

    fix application name: s/c2cgeoportal/project/
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cgeoportal/trunk@1422 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

--- c2cgeoportal/scripts/manage_db.py
+++ c2cgeoportal/scripts/manage_db.py
@@ -3,7 +3,7 @@ from migrate.versioning.shell import main
 from pyramid.paster import get_app
 
 def run():
-    app = get_app("c2cgeoportal/production.ini", "c2cgeoportal")
+    app = get_app("c2cgeoportal/production.ini", "project")
     db = app.registry.settings['sqlalchemy.url']
 
     main(url=db, debug='False', repository='c2cgeoportal/c2cgeoportal/migration')
