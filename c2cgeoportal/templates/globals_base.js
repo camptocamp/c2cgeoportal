fa80e1ba404d58a1d39e0a1a61606ff6a5cd8254
Éric Lemoine <eric.lemoine@camptocamp.com>
2011-10-10 07:59:00 +0000

    move project-specific Mako templates out of c2cgeoportal, r=sbrunner
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cgeoportal/trunk@1453 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

---
+++ c2cgeoportal/templates/globals_base.js
@@ -0,0 +1,50 @@
+<%!
+    import urlparse
+%>
+// this code may execute before the App namespace exists. If it
+// does not exist, we create it ourselves.
+if(!window.App) App = {};
+App["fullTextSearchURL"] = "${request.route_url('fulltextsearch', path='')}";
+% if len(tilecache_url) == 0:
+App["tilecacheURL"] = "${request.route_url('tilecache', path='')}";
+% else:
+App["tilecacheURL"] = '${tilecache_url}';
+% endif
+App["mapserverproxyURL"] = "${request.route_url('mapserverproxy', path='')}";
+App["printURL"] = "${request.route_url('printproxy', path='')}";
+App["csvURL"] = "${request.route_url('csvecho')}";
+App["loginURL"] = "${request.route_url('login')}";
+App["logoutURL"] = "${request.route_url('logout')}";
+App["overwiewImageType"] = "${overwiewimage_type}";
+App["overwiewImage"] = "${request.static_url(overwiewimage)}";
+App["overwiewImageBounds"] = ${overwiewimage_bounds};
+App["overwiewImageSize"] = ${overwiewimage_size};
+App["lang"] = "${lang}";
+% if user:
+App["user"] = "${user.username}";
+App["extent"] = ${user.role.jsextent};
+% else:
+App["extent"] = ${default_initial_extent | n};
+% endif
+App["restrictedExtent"] = ${restricted_extent | n};
+/*${themesError | n}*/
+App["themes"] = {
+    "local": ${themes | n}
+% if external_themes:
+    , "external": ${external_themes | n}
+% endif
+};
+App["default_themes"] = ${default_themes | n};
+App["HelpURL"] = "${request.static_url('c2cgeoportal:static/app/pdf/Hilfedokument_geoag.pdf')}";
+App["queryBuilderLayer"] = "${query_builder_layer}";
+% if encodedLayers_plan and encodedLayers_plan_color and encodedLayers_ortho:
+App["encodedLayers"] = {
+    "plan": ${encodedLayers_plan | n},
+    "plan_color": ${encodedLayers_plan_color | n},
+    "ortho": ${encodedLayers_ortho | n}
+};
+App["functionality"] = ${functionality | n};
+App["functionalities"] = ${functionalities | n};
+% endif
+<%block name="globals">\
+<%/block>\
