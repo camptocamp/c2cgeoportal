c6b18aa7e54761de6986cf2b66f52c98bc1638ee
Stéphane Brunner <stephane.brunner@camptocamp.com>
2011-10-26 14:17:31 +0000

    go back
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cgeoportal/trunk@1589 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

--- c2cgeoportal/forms.py
+++ c2cgeoportal/forms.py
@@ -203,7 +203,6 @@ class LayerCheckBoxTreeSet(CheckBoxTreeSet):
 
     def render_organisational_item(self, item, depth):
         if item in self.tree_items:
-            print item.name
             self.tree_items.remove(item)
 
         result = """
@@ -256,7 +255,6 @@ class LayerCheckBoxTreeSet(CheckBoxTreeSet):
         result = ""
         for item in themes:
             result += self.render_item(item, 1)
-            self.tree_items.remove(item)
 
         # add unlinked layers 
         if len(self.tree_items) >= 0:
