858cd9d3b0d4d4ee626b716b2c57870e100e2084
Bruno Binet <bruno.binet@camptocamp.com>
2011-11-15 17:25:12 +0000

    fix db url override in tests
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cgeoportal/trunk@1669 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

--- c2cgeoportal/tests/__init__.py
+++ c2cgeoportal/tests/__init__.py
@@ -7,8 +7,6 @@ import os
 from ConfigParser import ConfigParser
 
 from paste.deploy import appconfig
-import sqlahelper
-from sqlalchemy import create_engine
 
 curdir = os.path.dirname(os.path.abspath(__file__))
 configfile = os.path.realpath(os.path.join(curdir, 'test.ini'))
@@ -17,12 +15,15 @@ cfg = ConfigParser()
 cfg.read(configfile)
 sqlalchemy_url = cfg.get('test', 'sqlalchemy.url')
 mapserv_url = cfg.get('test', 'mapserv.url')
-sqlahelper.add_engine(create_engine(sqlalchemy_url))
 
 def setUpModule():
     import c2cgeoportal
     c2cgeoportal.schema = 'main'
     c2cgeoportal.srid = 21781
+
+    import sqlahelper
+    from sqlalchemy import create_engine
+    sqlahelper.add_engine(create_engine(sqlalchemy_url))
 
     from c2cgeoportal.models import Base
     Base.metadata.create_all()
@@ -30,3 +31,6 @@ def setUpModule():
 def tearDownModule():
     from c2cgeoportal.models import Base
     Base.metadata.drop_all(checkfirst=True)
+
+    import sqlahelper
+    sqlahelper.reset()
