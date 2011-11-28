65d08f15e7824c91b8b8fbb1ce9716ce9a5f3d81
Alex Saunier <alexandre.saunier@camptocamp.com>
2011-11-28 14:29:35 +0000

    fixed default value in layer.isChecked migration script, r=sbrunner
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cgeoportal/trunk@1714 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

--- c2cgeoportal/migration/versions/002_Add_isChecked_column.py
+++ c2cgeoportal/migration/versions/002_Add_isChecked_column.py
@@ -5,7 +5,7 @@ from c2cgeoportal import schema
 def upgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
     layer = Table('layer', meta, schema=schema, autoload=True)
-    Column('isChecked', types.Boolean).create(layer)
+    Column('isChecked', types.Boolean, default=True).create(layer)
 
 def downgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
