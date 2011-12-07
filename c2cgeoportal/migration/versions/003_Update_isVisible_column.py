1fc19c0f6516ab75c511759c19d57a861df3f8c8
Stéphane Brunner <stephane.brunner@camptocamp.com>
2011-12-07 10:05:48 +0000

    fix script to don't use the model
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cgeoportal/trunk@1746 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

--- c2cgeoportal/migration/versions/003_Update_isVisible_column.py
+++ c2cgeoportal/migration/versions/003_Update_isVisible_column.py
@@ -1,11 +1,12 @@
-from c2cgeoportal.models import DBSession, Layer
-import transaction
+from sqlalchemy import MetaData, Table 
+
+from c2cgeoportal import schema
 
 def upgrade(migrate_engine):
-    layers = DBSession.query(Layer).all()
-    for layer in layers:
-        layer.isVisible = True
-    transaction.commit()
+    meta = MetaData(bind=migrate_engine)
+    layer = Table('layer', meta, schema=schema, autoload=True)
+    conn = migrate_engine.connect()
+    conn.execute(layer.update().values(isVisible = True))
 
 def downgrade(migrate_engine):
     pass
