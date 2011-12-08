49d1dede3d5735e5755660c6a14d751b46ebbffa
Bruno Binet <bruno.binet@camptocamp.com>
2011-12-08 10:00:55 +0000

    remove the need of passing srid when applying the update template
    the srid was needed in the 'update' template only because it was used in the CONST_README.txt, so this patch remove the replacement of srid in the readme.
    
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cgeoportal/trunk@1763 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

--- paste_templates/__init__.py
+++ paste_templates/__init__.py
@@ -2,22 +2,19 @@
 
 from paste.script import templates
 
-vars = [
-    templates.var('srid', 'A Spatial Reference System Identifier (e.g. 21781)'),
-    ]
 
 class TemplateCreate(templates.Template):
     _template_dir = 'create'
     summary = 'Template used to create a c2cgeoportal project'
-    vars = vars
-    vars.append(
+    vars = [
+        templates.var('srid', 'A Spatial Reference System Identifier (e.g. 21781)'),
         templates.var(
             'version_table_suffix',
             'The sqlalchemy-migrate version table name suffix (this table ' \
-                'will be named: "version_${package}${version_table_suffix}")'))
+                'will be named: "version_${package}${version_table_suffix}")'),
+        ]
 
 class TemplateUpdate(templates.Template):
     _template_dir = 'update'
     summary = 'Template used to update a c2cgeoportal project'
-    vars = vars
 
