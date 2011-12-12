7bd3e64a0f8be9319f080c3b0e1f8630f4c1df7b
Éric Lemoine <eric.lemoine@camptocamp.com>
2011-12-12 13:45:04 +0000

    add a shell script for updating the c2cgeoportal doc on docs.camptocamp.net
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cgeoportal/trunk@1784 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

---
+++ doc/update_online.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#
+# This script updates the c2cgeoportal HTML docs available at 
+# http://docs.camptocamp.net/c2cgeoportal/ 
+#
+# This script is to be run on the doc.camptocamp.net server,
+# from the c2cgeoportal/doc directory.
+#
+# To run the script change dir to c2cgeoportal/doc and do:
+#   ./update_online.sh
+#
+# Possible Apache config:
+#
+# Alias /c2cgeoportal "/var/www/docs.camptocamp.net/htdocs/c2cgeoportal/html"
+# <Directory "/var/www/docs.camptocamp.net/htdocs/c2cgeoportal/html">
+#     Options Indexes FollowSymLinks MultiViews
+#     AllowOverride None
+#     Order allow,deny
+#     allow from all
+# </Directory>
+#
+
+# BUILDDIR is where the HTML files are generated
+BUILDDIR=/var/www/docs.camptocamp.net/htdocs/c2cgeoportal
+
+# create the build dir if it doesn't exist
+if [[ ! -d ${BUILDDIR} ]]; then
+    mkdir -p ${BUILDDIR}
+fi
+
+# get the latest files
+svn up
+
+# create a virtual env if none exists already
+if [[ ! -d env ]]; then
+    curl http://mapfish.org/downloads/virtualenv-1.4.5.py | python - --no-site-packages --distribute env
+fi
+
+# install or update Sphinx
+./env/bin/pip install Sphinx
+
+make SPHINXBUILD=./env/bin/sphinx-build BUILDDIR=${BUILDDIR} clean html
+
+exit 0
