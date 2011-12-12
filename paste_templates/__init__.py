95bfb563a2f6725aa677a3b9f7bd0239ffbe6fb8
Bruno Binet <bruno.binet@gmail.com>
2011-12-12 15:27:44 +0100

    remove version_table_suffix
    if some projects have the same ${package} name, the version_table parameter
    will have to be manually updated for all these projects.

--- paste_templates/__init__.py
+++ paste_templates/__init__.py
@@ -8,10 +8,6 @@ class TemplateCreate(templates.Template):
     summary = 'Template used to create a c2cgeoportal project'
     vars = [
         templates.var('srid', 'A Spatial Reference System Identifier (e.g. 21781)'),
-        templates.var(
-            'version_table_suffix',
-            'The sqlalchemy-migrate version table name suffix (this table ' \
-                'will be named: "version_${package}${version_table_suffix}")'),
         ]
 
 class TemplateUpdate(templates.Template):
