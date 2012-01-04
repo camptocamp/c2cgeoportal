02a77ebb7cb6d2875db2334bd80353b506423796
Éric Lemoine <eric.lemoine@gmail.com>
2012-01-04 21:36:30 +0100

    remove empty test files

--- c2cgeoportal/tests/functional/test_exportcsv.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from unittest import TestCase
-
-class TestExportcsvView(TestCase):
-
-    def test_index(self):
-        pass
--- c2cgeoportal/tests/functional/test_objs.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from unittest import TestCase
-
-class TestObjsView(TestCase):
-    def test_index(self):
-        pass
