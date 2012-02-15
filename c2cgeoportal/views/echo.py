30b679f412c12ab6b1437eb212630a863bc08f0b
Tom Payne <tom.payne@camptocamp.com>
2012-02-15 12:28:37 +0100

    Wrap base64 encoded chunks in JSON

--- c2cgeoportal/views/echo.py
+++ c2cgeoportal/views/echo.py
@@ -7,23 +7,26 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 
-def base64_encode_chunks(file, chunk_size=57):
+def json_base64_encode_chunks(file, chunk_size=65536):
     """
-    Generate base64 encoded lines of up to 76 (== 57 * 8 / 6) characters, according to RFC2045.
+    Generate a JSON-wrapped base64-encoded string.
     See http://en.wikipedia.org/wiki/Base64
     """
+    yield '{"data":"'
     while True:
         line = file.read(chunk_size)
         if not line:
             break
-        yield b64encode(line) + '\n'
+        yield b64encode(line)
+    yield '"}'
 
 
 @view_config(route_name='echo')
 def echo(request):
     """
     Echo an uploaded file back to the client as an text/html document so it can be handled by Ext.
-    The response is base64 encoded to ensure that there are no special HTML characters or charset problems.
+    The response is JSON-wrapped and base64-encoded to ensure that there are no special HTML characters or charset problems and so that braindead ext doesn't barf on it.
+    We use an iterator to avoid loading the whole file into memory.
     See http://docs.sencha.com/ext-js/3-4/#!/api/Ext.form.BasicForm-cfg-fileUpload
     """
     if request.method != 'POST':
@@ -33,6 +36,6 @@ def echo(request):
     except KeyError:
         raise HTTPBadRequest()
     response = Response()
-    response.app_iter = base64_encode_chunks(file.file)
+    response.app_iter = json_base64_encode_chunks(file.file)
     response.content_type = 'text/html'
     return response
