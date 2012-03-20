014cb34d11dcf1ea0a60e88308ddb34bd834e6f7
Tom Payne <twpayne@gmail.com>
2012-03-20 18:21:35 +0100

    Don't attempt to chunk echoed files (it breaks the base64 encoding)

--- c2cgeoportal/views/echo.py
+++ c2cgeoportal/views/echo.py
@@ -6,17 +6,13 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 
-def json_base64_encode_chunks(filename, file, chunk_size=65536):
+def json_base64_encode(filename, file):
     """
     Generate a JSON-wrapped base64-encoded string.
     See http://en.wikipedia.org/wiki/Base64
     """
     yield '{"filename":%s,"data":"' % (json.dumps(filename),)
-    while True:
-        line = file.read(chunk_size)
-        if not line:
-            break
-        yield b64encode(line)
+    yield b64encode(file.read())
     yield '","success":true}'
 
 
@@ -30,7 +26,7 @@ def echo(request):
     special HTML characters or charset problems and so that braindead ext
     doesn't barf on it.
 
-    We use an iterator to avoid loading the whole file into memory.  See
+    See:
     http://docs.sencha.com/ext-js/3-4/#!/api/Ext.form.BasicForm-cfg-fileUpload
     """
     if request.method != 'POST':
@@ -40,6 +36,6 @@ def echo(request):
     except KeyError:
         return HTTPBadRequest()
     response = Response()
-    response.app_iter = json_base64_encode_chunks(file.filename, file.file)
+    response.app_iter = json_base64_encode(file.filename, file.file)
     response.content_type = 'text/html'
     return response
