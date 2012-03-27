944dd68f038d138d03128a383f04fe2e4e220d8e
Éric Lemoine <eric.lemoine@gmail.com>
2012-03-27 10:22:15 +0200

    secure the layers read_many view, and do some necessary refactoring along the way, this still requires sbrunner's GeoAlchemy aggregate branch

--- c2cgeoportal/tests/functional/test_layers.py
+++ c2cgeoportal/tests/functional/test_layers.py
@@ -10,17 +10,29 @@ class TestLayers(TestCase):
 
     def setUp(self):
         import sqlahelper
+        import transaction
+        from c2cgeoportal.models import DBSession, Role, User
         from c2cgeoportal.lib.dbreflection import init
 
         self.tables = []
         self.layer_ids = []
+
+        self.role = Role(name=u'__test_role')
+        self.user = User(username=u'__test_user',
+                         password=u'__test_user',
+                         role=self.role
+                    )
+
+        DBSession.add(self.user)
+        transaction.commit()
 
         engine = sqlahelper.get_engine()
         init(engine)
 
     def tearDown(self):
         import transaction
-        from c2cgeoportal.models import DBSession, Layer, TreeItem
+        from c2cgeoportal.models import (DBSession, Role, User,
+                                         Layer, TreeItem, RestrictionArea)
 
         transaction.commit()
 
@@ -33,16 +45,26 @@ class TestLayers(TestCase):
             layer = DBSession.query(Layer).get(i)
             DBSession.delete(layer)
 
+        DBSession.query(User).filter(
+                User.username == '__test_user').delete()
+        DBSession.query(Role).filter(
+                Role.name == '__test_role').delete()
+        DBSession.query(RestrictionArea).filter(RestrictionArea.name == '__test_ra').delete()
+
         transaction.commit()
 
     def _create_layer(self, id):
+        """ This function is central for this test class. It creates
+        a layer with two features, and associates a restriction area
+        to it. """
         import transaction
         import sqlahelper
         from sqlalchemy import func
         from sqlalchemy import Column, Table, MetaData, types
         from sqlalchemy.ext.declarative import declarative_base
-        from geoalchemy import GeometryDDL, GeometryExtensionColumn, Point
-        from c2cgeoportal.models import DBSession, Layer
+        from geoalchemy import (GeometryDDL, GeometryExtensionColumn,
+                                Point, WKTSpatialElement)
+        from c2cgeoportal.models import DBSession, Layer, RestrictionArea
 
         engine = sqlahelper.get_engine()
         Base = declarative_base(bind=engine)
@@ -52,7 +74,7 @@ class TestLayers(TestCase):
         table = Table(tablename, Base.metadata,
                 Column('id', types.Integer, primary_key=True),
                 Column('name', types.Unicode),
-                GeometryExtensionColumn('geom', Point),
+                GeometryExtensionColumn('geom', Point(srid=21781)),
                 schema='public')
         GeometryDDL(table)
         table.create()
@@ -60,27 +82,43 @@ class TestLayers(TestCase):
 
         ins = table.insert().values(
                 name='foo',
-                geom=func.ST_GeomFromText('POINT(5 45)', 4326))
+                geom=func.ST_GeomFromText('POINT(5 45)', 21781))
         engine.connect().execute(ins)
         ins = table.insert().values(
                 name='bar',
-                geom=func.ST_GeomFromText('POINT(6 46)', 4326))
+                geom=func.ST_GeomFromText('POINT(6 46)', 21781))
         engine.connect().execute(ins)
 
         layer = Layer()
         layer.id = id
         layer.geoTable = tablename
-        DBSession.add(layer)
+        layer.public = False
+
+        ra = RestrictionArea()
+        ra.name = u'__test_ra'
+        ra.layers = [layer]
+        ra.roles = [self.role]
+        poly = 'POLYGON((4 44, 4 46, 6 46, 6 44, 4 44))'
+        ra.area = WKTSpatialElement(poly, srid=21781)
+
+        DBSession.add(ra)
+
         self.layer_ids.append(id)
 
         transaction.commit()
 
-    def _get_request(self, layerid):
+    def _get_request(self, layerid, username=None):
+        from c2cgeoportal.models import DBSession, User
         request = testing.DummyRequest()
         request.matchdict = {'layer_id': str(layerid)}
+        if username is not None:
+            request.user = DBSession.query(User).filter_by(
+                                username=username).one()
+        else:
+            request.user = None
         return request
 
-    def test_read_many(self):
+    def test_read_many_no_auth(self):
         from geojson.feature import FeatureCollection
         from c2cgeoportal.views.layers import read_many
 
@@ -90,7 +128,19 @@ class TestLayers(TestCase):
 
         collection = read_many(request)
         self.assertTrue(isinstance(collection, FeatureCollection))
-        self.assertEquals(len(collection.features), 2)
+        self.assertEquals(len(collection.features), 0)
+
+    def test_read_many_auth(self):
+        from geojson.feature import FeatureCollection
+        from c2cgeoportal.views.layers import read_many
+
+        layer_id = 1
+        self._create_layer(layer_id)
+        request = self._get_request(layer_id, username=u'__test_user')
+
+        collection = read_many(request)
+        self.assertTrue(isinstance(collection, FeatureCollection))
+        self.assertEquals(len(collection.features), 1)
 
     def test_read_one(self):
         from geojson.feature import Feature
@@ -181,14 +231,11 @@ class TestLayers(TestCase):
         self._create_layer(11)
 
         self.assertTrue(True)
-        request = self._get_request('9,10,11')
+        request = self._get_request('9,10,11', username=u'__test_user')
 
         collection = read_many(request)
         self.assertTrue(isinstance(collection, FeatureCollection))
-        self.assertEquals(len(collection.features), 6)
+        self.assertEquals(len(collection.features), 3)
         self.assertEquals(collection.features[0].properties['__layer_id__'], 9)
-        self.assertEquals(collection.features[1].properties['__layer_id__'], 9)
-        self.assertEquals(collection.features[2].properties['__layer_id__'], 10)
-        self.assertEquals(collection.features[3].properties['__layer_id__'], 10)
-        self.assertEquals(collection.features[4].properties['__layer_id__'], 11)
-        self.assertEquals(collection.features[5].properties['__layer_id__'], 11)
+        self.assertEquals(collection.features[1].properties['__layer_id__'], 10)
+        self.assertEquals(collection.features[2].properties['__layer_id__'], 11)
--- c2cgeoportal/views/layers.py
+++ c2cgeoportal/views/layers.py
@@ -1,44 +1,21 @@
 from pyramid.httpexceptions import (HTTPInternalServerError, HTTPNotFound,
                                     HTTPBadRequest)
 from pyramid.view import view_config
+from pyramid.security import authenticated_userid
 
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from sqlalchemy.sql import and_
 
-from geoalchemy import Geometry
+from geoalchemy import functions, Geometry, DBSpatialElement
 
 from geojson.feature import FeatureCollection
 
 from papyrus.renderers import XSD
-from papyrus.protocol import Protocol
+from papyrus.protocol import Protocol, create_filter
 
 from c2cgeoportal.lib.dbreflection import get_class
-from c2cgeoportal.models import DBSession, Layer
-
-def _get_class(layer_id):
-    """ Return an SQLAlchemy mapped class for the layer
-    identified by ``layer_id``. """
-    layer_id = int(layer_id)
-    try:
-        table_name, = DBSession.query(Layer.geoTable) \
-                               .filter(Layer.id == layer_id) \
-                               .one()
-    except NoResultFound:
-        raise HTTPNotFound("Layer not found")
-    except MultipleResultsFound: # pragma: no cover
-        raise HTTPInternalServerError() # pragma: no cover
-    return get_class(str(table_name))
-
-
-def _get_classes(request):
-    """ A generator function that yields ``(layer_id, class)``
-    tuples. """
-    try:
-        layer_ids = (int(layer_id) for layer_id in \
-                         request.matchdict['layer_id'].split(',') if layer_id)
-        for layer_id in layer_ids:
-            yield (layer_id, _get_class(layer_id))
-    except ValueError:
-        raise HTTPBadRequest() # pragma: no cover
+from c2cgeoportal.models import (DBSession, Layer, RestrictionArea,
+                                 User, Role)
 
 
 def _get_geom_attr(mapped_class):
@@ -47,69 +24,125 @@ def _get_geom_attr(mapped_class):
     for column in mapped_class.__table__.columns:
         if isinstance(column.type, Geometry):
             return column.name
-    raise HTTPInternalServerError() # pragma: no cover
+    raise HTTPInternalServerError()  # pragma: no cover
 
 
-def _get_protocols(request):
-    """ A generator function that yields ``(layer_id, protocol)``
-    tuples. """
-    for layer_id, cls in _get_classes(request):
-        geom_attr = _get_geom_attr(cls)
-        yield (layer_id, Protocol(DBSession, cls, geom_attr))
-
-
-def _get_protocol(request):
+def _get_layer(layer_id):
+    """ Return a ``Layer`` object for ``layer_id``. """
+    layer_id = int(layer_id)
     try:
-        return next(p for _, p in _get_protocols(request))
-    except StopIteration: # pragma: no cover
-        raise HTTPInternalServerError() # pragma: no cover
+        layer, geo_table = DBSession.query(Layer, Layer.geoTable) \
+                                    .filter(Layer.id == layer_id) \
+                                    .one()
+    except NoResultFound:
+        raise HTTPNotFound("Layer %d not found" % layer_id)
+    except MultipleResultsFound:  # pragma: no cover
+        raise HTTPInternalServerError()  # pragma: no cover
+    if not geo_table:
+        raise HTTPNotFound("Layer %d has no geo table" % layer_id)
+    return layer
+
+
+def _get_layers_for_request(request):
+    """ A generator function that yields ``Layer`` objects based
+    on the layer ids found in the ``layer_id`` matchdict. """
+    try:
+        layer_ids = (int(layer_id) for layer_id in \
+                         request.matchdict['layer_id'].split(',') if layer_id)
+        for layer_id in layer_ids:
+            yield _get_layer(layer_id)
+    except ValueError:
+        raise HTTPBadRequest()  # pragma: no cover
+
+
+def _get_protocol_for_layer(layer):
+    """ Returns a papyrus ``Protocol`` for the ``Layer`` object. """
+    cls = get_class(str(layer.geoTable))
+    return Protocol(DBSession, cls, _get_geom_attr(cls))
+
+
+def _get_protocol_for_request(request):
+    """ Returns a papyrus ``Protocol`` for the first layer
+    found in the ``layer_id`` matchdict. """
+    layer = next(_get_layers_for_request(request))
+    return _get_protocol_for_layer(layer)
+
+
+def _get_class_for_request(request):
+    """ Return an SQLAlchemy mapped class for the first
+    found in the ``layer_id`` matchdict. """
+    layer = next(_get_layers_for_request(request))
+    return get_class(str(layer.geoTable))
+
+
+def _proto_read(layer, request):
+    """ Read features for the layer based on the request. """
+    if layer.public:
+        return _get_protocol_for_layer(layer).read(request)
+    if request.user is None:
+        return FeatureCollection([])
+    user = request.user
+    proto = _get_protocol_for_layer(layer)
+    cls = proto.mapped_class
+    geom_attr = _get_geom_attr(cls)
+    polygon = DBSession.query(
+                           RestrictionArea.area.collect) \
+                       .join(RestrictionArea.roles) \
+                       .join(RestrictionArea.layers) \
+                       .filter(RestrictionArea.area.area > 0) \
+                       .filter(Role.id == user.role.id) \
+                       .filter(Layer.id == layer.id).scalar()
+    polygon = DBSpatialElement(polygon)
+    filter_ = and_(create_filter(request, cls, geom_attr),
+                   polygon.gcontains(getattr(cls, geom_attr))
+                   )
+    return proto.read(request, filter=filter_)
 
 
 @view_config(route_name='layers_read_many', renderer='geojson')
 def read_many(request):
     features = []
-    for layer_id, proto in _get_protocols(request):
-        collection = proto.read(request)
-        for feature in collection.features:
-            feature.properties['__layer_id__'] = layer_id
-            features.append(feature)
+    for layer in _get_layers_for_request(request):
+        for f in _proto_read(layer, request).features:
+            f.properties['__layer_id__'] = layer.id
+            features.append(f)
     return FeatureCollection(features)
 
 
 @view_config(route_name='layers_read_one', renderer='geojson')
 def read_one(request):
     feature_id = request.matchdict.get('feature_id', None)
-    protocol = _get_protocol(request)
+    protocol = _get_protocol_for_request(request)
     return protocol.read(request, id=feature_id)
 
 
 @view_config(route_name='layers_count', renderer='string')
 def count(request):
-    protocol = _get_protocol(request)
+    protocol = _get_protocol_for_request(request)
     return protocol.count(request)
 
 
 @view_config(route_name='layers_create', renderer='geojson')
 def create(request):
-    protocol = _get_protocol(request)
+    protocol = _get_protocol_for_request(request)
     return protocol.create(request)
 
 
 @view_config(route_name='layers_update', renderer='geojson')
 def update(request):
     feature_id = request.matchdict.get('feature_id', None)
-    protocol = _get_protocol(request)
+    protocol = _get_protocol_for_request(request)
     return protocol.update(request, feature_id)
 
 
 @view_config(route_name='layers_delete')
 def delete(request):
     feature_id = request.matchdict.get('feature_id', None)
-    protocol = _get_protocol(request)
+    protocol = _get_protocol_for_request(request)
     return protocol.delete(request, feature_id)
 
 
 @view_config(route_name='layers_metadata', renderer='xsd')
 def metadata(request):
-    mapped_class = _get_class(request.matchdict['layer_id'])
+    mapped_class = _get_class_for_request(request)
     return mapped_class.__table__
