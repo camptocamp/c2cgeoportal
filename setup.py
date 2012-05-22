586916300f5681e6aa95170af92a1f25ea3c424f
Éric Lemoine <eric.lemoine@gmail.com>
2012-05-22 14:56:54 +0200

    pyramid 1.3 upgrade - move the scaffolds dir into c2cgeoportal

--- setup.py
+++ setup.py
@@ -70,8 +70,8 @@ setup(name='c2cgeoportal',
             'manage_db = c2cgeoportal.scripts.manage_db:main',
         ],
         'pyramid.scaffold': [
-            'c2cgeoportal_create = scaffolds:TemplateCreate',
-            'c2cgeoportal_update = scaffolds:TemplateUpdate',
+            'c2cgeoportal_create = c2cgeoportal.scaffolds:TemplateCreate',
+            'c2cgeoportal_update = c2cgeoportal.scaffolds:TemplateUpdate',
         ],
         'fanstatic.libraries': [
             'admin = c2cgeoportal.forms:fanstatic_lib',
