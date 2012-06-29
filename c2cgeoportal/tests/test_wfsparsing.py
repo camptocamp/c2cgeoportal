9fca6bca3617af873ea09daea57a60a3193bcc2c
Stéphane Brunner <stephane.brunner@camptocamp.com>
2012-06-29 18:19:37 +0200

    complets wfsparsing tests

--- c2cgeoportal/tests/test_wfsparsing.py
+++ c2cgeoportal/tests/test_wfsparsing.py
@@ -1,28 +1,41 @@
-def test_is_get_feature():
-    from c2cgeoportal.lib.wfsparsing import is_get_feature
-    from c2cgeoportal.tests.xmlstr import getfeature
-    assert is_get_feature(getfeature)
+# -*- coding: utf-8 -*-
+from unittest import TestCase
 
-def test_is_get_feature_not():
-    from c2cgeoportal.lib.wfsparsing import is_get_feature
-    assert not is_get_feature('<is_not>foo</is_not>')
+class TestEntryView(TestCase):
+    def test_is_get_feature(self):
+        from c2cgeoportal.lib.wfsparsing import is_get_feature
+        from c2cgeoportal.tests.xmlstr import getfeature
+        assert is_get_feature(getfeature)
 
-def test_limit_featurecollection_outlimit():
-    from xml.etree.ElementTree import fromstring
-    from c2cgeoportal.lib.wfsparsing import limit_featurecollection
-    from c2cgeoportal.tests.xmlstr import featurecollection_outlimit
-    content = limit_featurecollection(featurecollection_outlimit)
-    collection = fromstring(content.encode('utf-8'))
-    features = collection.findall(
-            '{http://www.opengis.net/gml}featureMember') 
-    assert len(features) == 200
+    def test_is_get_feature_not(self):
+        from c2cgeoportal.lib.wfsparsing import is_get_feature
+        assert not is_get_feature('<is_not>foo</is_not>')
 
-def test_limit_featurecollection_inlimit():
-    from xml.etree.ElementTree import fromstring
-    from c2cgeoportal.lib.wfsparsing import limit_featurecollection
-    from c2cgeoportal.tests.xmlstr import featurecollection_inlimit
-    content = limit_featurecollection(featurecollection_inlimit)
-    collection = fromstring(content.encode('utf-8'))
-    features = collection.findall(
-            '{http://www.opengis.net/gml}featureMember') 
-    assert len(features) == 199
+    def test_limit_featurecollection_outlimit(self):
+        from xml.etree.ElementTree import fromstring
+        from c2cgeoportal.lib.wfsparsing import limit_featurecollection
+        from c2cgeoportal.tests.xmlstr import featurecollection_outlimit
+        content = limit_featurecollection(featurecollection_outlimit)
+        collection = fromstring(content.encode('utf-8'))
+        features = collection.findall(
+                '{http://www.opengis.net/gml}featureMember') 
+        self.assertEquals(len(features), 200)
+
+        from xml.etree.ElementTree import fromstring
+        from c2cgeoportal.lib.wfsparsing import limit_featurecollection
+        from c2cgeoportal.tests.xmlstr import featurecollection_outlimit
+        content = limit_featurecollection(featurecollection_outlimit, limit=2)
+        collection = fromstring(content.encode('utf-8'))
+        features = collection.findall(
+                '{http://www.opengis.net/gml}featureMember') 
+        self.assertEquals(len(features), 2)
+
+    def test_limit_featurecollection_inlimit(self):
+        from xml.etree.ElementTree import fromstring
+        from c2cgeoportal.lib.wfsparsing import limit_featurecollection
+        from c2cgeoportal.tests.xmlstr import featurecollection_inlimit
+        content = limit_featurecollection(featurecollection_inlimit)
+        collection = fromstring(content.encode('utf-8'))
+        features = collection.findall(
+                '{http://www.opengis.net/gml}featureMember') 
+        self.assertEquals(len(features), 199)
