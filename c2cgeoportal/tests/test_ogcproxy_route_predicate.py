6167baee757ef39b5b66504b295114f9b4d77ab7
Éric Lemoine <eric.lemoine@gmail.com>
2012-08-20 11:08:57 +0200

    add tests for ogcproxy route predicate

---
+++ c2cgeoportal/tests/test_ogcproxy_route_predicate.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+def test_no_url():
+    from c2cgeoportal import ogcproxy_route_predicate
+    from pyramid.request import Request
+
+    request = Request.blank('/test')
+    ret = ogcproxy_route_predicate(None, request)
+    assert ret == False
+
+
+def test_mapserv_url():
+    from c2cgeoportal import ogcproxy_route_predicate
+    from pyramid.request import Request
+
+    request = Request.blank('/test?url=http://foo.com/mapserv')
+    ret = ogcproxy_route_predicate(None, request)
+    assert ret == False
+
+
+def test_non_mapserv_url():
+    from c2cgeoportal import ogcproxy_route_predicate
+    from pyramid.request import Request
+
+    request = Request.blank('/test?url=http://foo.com/wmts')
+    ret = ogcproxy_route_predicate(None, request)
+    assert ret == True
