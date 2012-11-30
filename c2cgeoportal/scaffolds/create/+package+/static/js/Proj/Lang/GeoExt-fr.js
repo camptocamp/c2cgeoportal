9fe62def69de26d5e01d09977c5347e282e59981
Stéphane Brunner <stephane.brunner@camptocamp.com>
2012-11-30 13:35:47 +0100

    Remove default l10n (moved to cgxp)

--- c2cgeoportal/scaffolds/create/+package+/static/js/Proj/Lang/GeoExt-fr.js
+++ c2cgeoportal/scaffolds/create/+package+/static/js/Proj/Lang/GeoExt-fr.js
@@ -2,15 +2,5 @@
  * @requires GeoExt/Lang.js
  */
 
-GeoExt.Lang.add("en", {
-    "cgxp.plugins.ContextualData.Tooltip.prototype": {
-        defaultTpl: "Coordonnées suisses : {coord_x} {coord_y}<br />" +
-            "WGS 84 : {wsg_x} {wsg_y}<br />",
-    },
-
-    "cgxp.plugins.ContextualData.ContextPopup.prototype": {
-        coordTpl: "<tr><td width=\"150\">Coordonnées suisses</td>" +
-            "<td>{coord_x} {coord_y}</td></tr>" +
-            "<tr><td>WGS 84</td><td>{wsg_x} {wsg_y}</td></tr>",
-    }
+GeoExt.Lang.add("fr", {
 });
