13455936de6e0868b2d92299b0c72b3d0b69af7c
Stéphane Brunner <stephane.brunner@camptocamp.com>
2012-12-03 13:38:16 +0100

    Starts version 1.3

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ tests_require = install_requires + [
     ]
 
 setup(name='c2cgeoportal',
-      version='1.2',
+      version='1.3',
       description='c2cgeoportal',
       long_description=README,
       classifiers=[
