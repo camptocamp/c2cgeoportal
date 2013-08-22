eb7f7248e87103af0fdef2b091862e1c09a3d772
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-08-22 12:23:14 +0200

    Do release 1.3.3.1

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ tests_require = install_requires + [
     ]
 
 setup(name='c2cgeoportal',
-      version='1.3.4',
+      version='1.3.3.1',
       description='c2cgeoportal',
       long_description=README,
       classifiers=[
