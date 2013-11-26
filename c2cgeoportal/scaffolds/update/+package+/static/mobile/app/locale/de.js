b0d4200cce0b4416d49271d3dc0f9ce10596350b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-11-26 16:35:13 +0100

    Move localisation in separate files

---
+++ c2cgeoportal/scaffolds/update/+package+/static/mobile/app/locale/de.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2011-2013 by Camptocamp SA
+ *
+ * CGXP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CGXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CGXP. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+OpenLayers.Util.extend(OpenLayers.Lang.de, {
+    // base layer switcher picker (you shouldn't remove this)
+    'layer_switcher.cancel': 'Abbrechen',
+    'layer_switcher.done': 'OK',
+
+    // themes
+    'theme_switcher.prefix': 'Thema: ',
+
+    // login/logout
+    'loginButtonText': 'Log in',
+    'welcomeText': '<p>Sie sind {username}.</p>',
+    'logoutButtonText': 'Log out',
+    'loginLabel': 'Login',
+    'passwordLabel': 'Passwort',
+    'loginSubmitButtonText': 'OK',
+    'loginCancelButtonText': 'Abbrechen',
+
+    // redirect to standard application
+    'redirect_msg': "Sie benutzen die mobile Version. Sie können auch" +
+        " die <a href='${'${url}'}'>Standardversion</a> benutzen.",
+    'close': "Schliessen"
+});
---
+++ c2cgeoportal/scaffolds/update/+package+/static/mobile/app/locale/fr.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2011-2013 by Camptocamp SA
+ *
+ * CGXP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CGXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CGXP. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+OpenLayers.Util.extend(OpenLayers.Lang.fr, {
+    // base layer switcher picker (you shouldn't remove this)
+    'layer_switcher.cancel': 'Annuler',
+    'layer_switcher.done': 'OK',
+    'layer_switcher.background': 'Fond',
+    'layer_switcher.data_layers': 'Couches de données',
+
+    // themes
+    'theme_switcher.prefix': 'Thème : ',
+
+    // login/logout
+    'loginButtonText': 'Me connecter',
+    'welcomeText': '<p>Vous êtes {username}.</p>',
+    'logoutButtonText': 'Me déconnecter',
+    'loginLabel': 'Nom',
+    'passwordLabel': 'Mot de passe',
+    'loginSubmitButtonText': 'Me connecter',
+    'loginCancelButtonText': 'Annuler',
+
+    // redirect to standard application
+    'redirect_msg': "Vous utilisez la version pour mobile. Vous pouvez aussi" +
+        " consulter la <a href='${'${url}'}'>version standard</a>.",
+    'close': "Fermer"
+});
