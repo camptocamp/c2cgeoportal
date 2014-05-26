abf0b9a979d23da2ac85858b4771788d152215af
Stéphane Brunner <stephane.brunner@camptocamp.com>
2014-05-26 10:23:59 +0200

    Start release 1.4.5

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ tests_require = install_requires + [
 
 setup(
     name='c2cgeoportal',
-    version='1.4.4',
+    version='1.4.5',
     description='c2cgeoportal',
     long_description=README,
     classifiers=[
