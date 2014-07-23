299e112996bfd716f5000215ddd03b8620558270
Stéphane Brunner <stephane.brunner@camptocamp.com>
2014-07-23 17:47:29 +0200

    layer.childLayers isn't always set

--- c2cgeoportal/scaffolds/update/+package+/static/mobile/app/controller/Main.js
+++ c2cgeoportal/scaffolds/update/+package+/static/mobile/app/controller/Main.js
@@ -131,10 +131,10 @@ Ext.define('App.controller.Main', {
             var theme = App.themes[i];
             for (var j = 0, jl = theme.allLayers.length; j < jl; j++ ) {
                 var layer = theme.allLayers[j];
+                var childLayers = layer.childLayers || [];
                 this.layers[layer.name] = layer;
-                for (var k = 0, kl = layer.childLayers.length; k < kl; k++ ) {
-                    var childLayer = layer.childLayers[k];
-                    this.layers[childLayer.name] = childLayer;
+                for (var k = 0, kl = childLayers.length; k < kl; k++ ) {
+                    this.layers[childLayers[k].name] = childLayers[k];
                 }
             }
         }
