30fd05571f0e2f15322e5392586090e4f99d680d
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-01-30 20:34:27 +0100

    Enable a pip cache

--- travis/pip.sh
+++ travis/pip.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Used to return the result code given by pip instance of by grep
-.build/venv/bin/pip --log /tmp/pip.log $* | grep '^Collecting '
+ARGS="--log /tmp/pip.log"
+PIP_VERSION=`.build/venv/bin/pip --version|awk '{print $2}' | awk -F. '{print $1}'`
+if [ ${PIP_VERSION} -ge 6 ]
+then
+    ARGS="${ARGS} --cache-dir /tmp/pip-cache"
+fi
+.build/venv/bin/pip ${ARGS} $* | grep '^Collecting '
 if [ ${PIPESTATUS[0]} -ne 0 ]
 then
     cat /tmp/pip.log
