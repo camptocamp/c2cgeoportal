9b1ad195a68ccbc637299a58b57bc6e03d7df3f0
Éric Lemoine <eric.lemoine@gmail.com>
2015-07-09 11:44:11 +0200

    Use virtualenv instead of /usr/bin/virtualenv on Travis

--- travis.mk
+++ travis.mk
@@ -1,5 +1,6 @@
 VARS_FILE ?= vars_travis.yaml
 VARS_FILES += vars_travis.yaml
 PIP_CMD = travis/pip.sh
+VIRTUALENV_CMD = virtualenv
 
 include Makefile
--- travis/build.mk
+++ travis/build.mk
@@ -8,6 +8,7 @@ REQUIREMENTS += -e /home/travis/build/camptocamp/c2cgeoportal
 PRINT_OUTPUT = /var/lib/tomcat7/webapps
 
 PIP_CMD = /home/travis/build/camptocamp/c2cgeoportal/travis/pip.sh
+VIRTUALENV_CMD = virtualenv
 
 TOMCAT_SERVICE_COMMAND =
 
