34a31ead43b095ef736806d46271f211d1d24260
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-08-07 18:05:13 +0200

    Create the database before the build

--- travis/build-new-project.sh
+++ travis/build-new-project.sh
@@ -20,12 +20,6 @@ git commit -q -m "Initial commit"
 sudo chmod 777 /var/lib/tomcat7/webapps
 
 make -f travis.mk cleanall
-make -f travis.mk build
-
-echo "Build complete"
-
-git add testgeomapfish/locale/*/LC_MESSAGES/testgeomapfish-server.po
-git commit -m "Add location"
 
 sudo -u postgres psql -c 'GRANT SELECT ON TABLE spatial_ref_sys TO "www-data"' geomapfish
 sudo -u postgres psql -c 'GRANT ALL ON TABLE geometry_columns TO "www-data"' geomapfish
@@ -33,8 +27,16 @@ sudo -u postgres psql -c "CREATE SCHEMA main;" geomapfish
 sudo -u postgres psql -c "CREATE SCHEMA main_static;" geomapfish
 sudo -u postgres psql -c 'GRANT ALL ON SCHEMA main TO "www-data"' geomapfish
 sudo -u postgres psql -c 'GRANT ALL ON SCHEMA main_static TO "www-data"' geomapfish
+make -f travis.mk .build/requirements.timestamp alembic.ini alembic_static.ini
+.build/venv/bin/alembic -c alembic.ini upgrade head
+.build/venv/bin/alembic -c alembic_static.ini upgrade head
 
-.build/venv/bin/alembic upgrade head
+make -f travis.mk build
+
+echo "Build complete"
+
+git add testgeomapfish/locale/*/LC_MESSAGES/testgeomapfish-server.po
+git commit -m "Add location"
 
 sudo touch /etc/apache2/sites-enabled/test.conf
 sudo chmod 666 /etc/apache2/sites-enabled/test.conf
