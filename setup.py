7a443e80ebdc496cf8a6ae7e9c67a8b952bb2017
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-10-16 15:06:51 +0200

    Start release 1.6.6

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ tests_require = install_requires + [
 
 setup(
     name="c2cgeoportal",
-    version="1.6.5",
+    version="1.6.6",
     description="c2cgeoportal",
     long_description=README,
     classifiers=[
