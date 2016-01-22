6a56913a71f1c70f63a26deed46d78d39586f32a
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-01-22 15:13:07 +0100

    fix

--- travis/doc.sh
+++ travis/doc.sh
@@ -1,30 +1,28 @@
 #!/bin/bash -e
 
 DOC=false
+BRANCH=master
 
 if [[ ${TRAVIS_BRANCH} =~ ^(master|[0-9]+.[0-9]+)$ ]] && [ ${TRAVIS_PULL_REQUEST} == false ]
 then
     DOC=true
+    BRANCH=${TRAVIS_BRANCH}
 fi
 
 
 echo == Build the doc ==
-if [ ${DOC} == false ]
-then
-    TRAVIS_BRANCH=master
-fi
 
 GIT_REV=`git log | head -n 1 | awk '{{print $2}}'`
 git fetch origin gh-pages:gh-pages
 git checkout gh-pages
 
-mkdir -p ${TRAVIS_BRANCH}
-rm -rf ${TRAVIS_BRANCH}/*
-mv doc/_build/html/* ${TRAVIS_BRANCH}
+mkdir -p ${BRANCH}
+rm -rf ${BRANCH}/*
+mv doc/_build/html/* ${BRANCH}
 
 if [ ${DOC} == true ]
 then
-    git add --all ${TRAVIS_BRANCH}
+    git add --all ${BRANCH}
     git commit -m "Update documentation for the revision ${TRAVIS_COMMIT}"
     git push origin gh-pages
 else
