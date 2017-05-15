3d0764a33fe10acc024c3f509fd7db8913620aba
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2017-05-15 10:11:34 +0200

    Fix idiot error

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2013-2016, Camptocamp SA
- All rights reserved.
+# All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
