7f7452492a53920e32617d6450177b341bf37d8e
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-06-15 12:31:39 +0200

    Make the phamtom js check working on Docker

--- c2cgeoportal/views/checker.py
+++ c2cgeoportal/views/checker.py
@@ -367,6 +367,7 @@ class Checker:  # pragma: no cover
         results = []
         for route in self.settings["phantomjs_routes"]:
             url = self.request.route_url(route["name"], _query=route.get("params", {}))
+            url, _ = build_url("Check", url, self.request)
 
             cmd = [executable_path, "--local-to-remote-url-access=true", checker_config_path, url]
 
