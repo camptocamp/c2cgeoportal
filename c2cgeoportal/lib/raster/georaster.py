3395a23a5c559b99f297832171be1b1cd2c2b2e1
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-07-25 08:06:14 +0200

    Don't shadow other variable

--- c2cgeoportal/lib/raster/georaster.py
+++ c2cgeoportal/lib/raster/georaster.py
@@ -58,29 +58,29 @@ class BTTile(Tile):
         self.resolution_y = None
 
     def get_value(self, x, y):
-        file = open(self.filename, "rb")
+        file_ = open(self.filename, "rb")
         if self.cols is None:
-            file.seek(10)
+            file_.seek(10)
             (self.cols, self.rows, self.dataSize, self.floatingPoint) = \
-                unpack("<LLhh", file.read(12))
+                unpack("<LLhh", file_.read(12))
             self.resolution_x = (self.max_x - self.min_x) / self.cols
             self.resolution_y = (self.max_y - self.min_y) / self.rows
 
         pos_x = int((x - self.min_x) / self.resolution_x)
         pos_y = int((y - self.min_y) / self.resolution_y)
-        file.seek(256 + (pos_y + pos_x * self.rows) * self.dataSize)
+        file_.seek(256 + (pos_y + pos_x * self.rows) * self.dataSize)
 
         if self.floatingPoint == 1:
-            val = unpack("<f", file.read(self.dataSize))[0]
+            val = unpack("<f", file_.read(self.dataSize))[0]
         else:
             if self.dataSize == 2:
                 format_ = "<h"
             else:
                 format_ = "<l"
-            data = file.read(self.dataSize)
+            data = file_.read(self.dataSize)
             val = unpack(format_, data)[0]
 
-        file.close()
+        file_.close()
         return val
 
 
