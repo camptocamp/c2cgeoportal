b84adf864714b17da8e7ac91e0500a4348bbf24c
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2017-08-30 11:09:06 +0200

    snapshot

--- commons/c2cgeoportal_commons/tests/__init__.py
+++ commons/c2cgeoportal_commons/tests/__init__.py
@@ -1,3 +1,97 @@
-"""Commons test package
-"""
+import unittest
+import pytest
+import transaction
+import sqlahelper
 
+from pyramid.config import Configurator
+from pyramid import testing
+
+from c2cgeoportal_commons.scripts.initializedb import init_db
+
+
+def app(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    In our case, this is only used for tests.
+    """
+    config = Configurator(settings=settings)
+    return config.make_wsgi_app()
+
+
+@pytest.fixture(scope='module')
+def dbsession(request):
+    config = testing.setUp(settings={
+        'sqlalchemy.url': 'postgresql://www-data:www-data@localhost:5432/c2cgeoportal',
+        'schema': 'main',
+        'parent_schema': '',
+        'srid': 3857
+    })
+    settings = config.get_settings()
+
+    from c2cgeoportal_commons.models import (
+        get_engine,
+        get_session_factory,
+        get_tm_session,
+        generate_mappers,
+        )
+
+    generate_mappers(settings)
+
+    engine = get_engine(settings)
+    sqlahelper.add_engine(engine)
+
+    init_db(engine, force=True)
+
+    session_factory = get_session_factory(engine)
+    session = get_tm_session(session_factory, transaction.manager)
+
+    import pydevd
+    pydevd.settrace()
+
+    return session
+
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        '''
+        self.config = testing.setUp(settings={
+            'sqlalchemy.url': 'sqlite:///:memory:'
+        })
+        self.config.include('.models')
+        settings = self.config.get_settings()
+
+        from .models import (
+            get_engine,
+            get_session_factory,
+            get_tm_session,
+            )
+
+        self.engine = get_engine(settings)
+        session_factory = get_session_factory(self.engine)
+
+        self.session = get_tm_session(session_factory, transaction.manager)
+        '''
+
+    def init_database(self):
+        from .models.meta import Base
+        Base.metadata.create_all(self.engine)
+
+    def tearDown(self):
+        '''
+        from .models.meta import Base
+
+        testing.tearDown()
+        transaction.abort()
+        Base.metadata.drop_all(self.engine)
+        '''
+
+
+def test_import(dbsession):
+    from c2cgeoportal_commons.models import main
+
+
+@pytest.mark.usefixtures("dbsession")
+class TestUser(BaseTest):
+
+    def test_all_users(self, dbsession):
+        from c2cgeoportal_commons.models.main import User
+        dbsession.query(User).all()
--- commons/setup.py
+++ commons/setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 
-url = ""
 version = "0.1.0"
-readme = open('README.rst').read()
+readme = open('README.md').read()
 
 setup(
     name="c2cgeoportal_commons",
@@ -11,18 +10,26 @@ setup(
     description="c2cgeoportal commons",
     long_description=readme,
     include_package_data=True,
-    author="c2c",
+    author="Camptocamp",
     author_email="info@camptocamp.com",
-    url=url,
     install_requires=[
         "sqlahelper",
         "papyrus",
     ],
-    download_url="{}/tarball/{}".format(url, version),
-    license="MIT",
+    extras_require={
+        'testing': [
+            'psycopg2',
+            'pytest',
+            'pytest-cov',
+            #'pydevd',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'initialize_db_main = c2cgeoportal_commons.scripts.initializedb:main',
+        ],
+        'paste.app_factory': [
+            'test_app = c2cgeoportal_commons.tests:app',
         ],
     },
 )
