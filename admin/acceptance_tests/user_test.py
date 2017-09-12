8a80f9ebee075db73c9c835e62554e6114fc9a36
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2017-09-12 12:09:11 +0200

    test for user update, affraid there is a mess with id
    (user db id and theses ones used in ihm)

--- admin/acceptance_tests/user_test.py
+++ admin/acceptance_tests/user_test.py
@@ -1,4 +1,5 @@
 import pytest
+import re
 from pyramid.testing import DummyRequest
 
 @pytest.fixture(scope='class')
@@ -13,6 +14,13 @@ def insertUsersTestData(dbsession):
     yield
     dbsession.rollback()
 
+def clean_form(form):
+    clean_form = form.replace('\n', ' ')
+    while re.search('  ', clean_form, re.MULTILINE):
+        clean_form = clean_form.replace('  ', ' ')
+    clean_form = clean_form.replace(' >', '>')
+    return clean_form
+
 @pytest.mark.usefixtures("insertUsersTestData", "transact")
 class TestUser():
     def test_view_index(self, dbsession):
@@ -26,17 +34,38 @@ class TestUser():
         from c2cgeoportal_admin.views.users import UserViews
         req = DummyRequest(dbsession=dbsession)
         req.matchdict.update({'id': '11'})
-        info = UserViews(req).edit()
-        import re
 
-        clean_form = info['form'].replace('\n', ' ')
-        while re.search('  ', clean_form, re.MULTILINE):
-            clean_form = clean_form.replace('  ', ' ')
-        clean_form = clean_form.replace(' >', '>')
-        inputs = re.findall('<input type="text" .*?>', clean_form)
+        form = clean_form(UserViews(req).edit()['form'])
 
+        inputs = re.findall('<input type="text" .*?>', form)
         assert inputs[0] == '<input type="text" name="username" value="babar_8" id="deformField3" class=" form-control "/>'
         assert inputs[3] == '<input type="text" name="email" value="mail8" id="deformField6" class=" form-control "/>'
+
+    def test_submit_update(self, dbsession):
+        from c2cgeoportal_admin.views.users import UserViews
+        post = {'__formid__': 'deform',
+                '_charset_': 'UTF-8',
+                'formsubmit': 'formsubmit',
+                'item_type': 'user',
+                'id': '11',
+                'username': 'new_name_withéàô',
+                'email': 'new_mail',
+                'role_name': '',
+                'is_password_changed': 'false',
+                '_password': 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+                'temp_password': ''}
+        req = DummyRequest(dbsession=dbsession, post=post)
+        req.matchdict.update({'id': '11'})
+
+        form = clean_form(UserViews(req).edit()['form'])
+
+        inputs = re.findall('<input type="text" .*?>', form)
+        assert inputs[0] == '<input type="text" name="username" value="new_name_withéàô" id="deformField3" class=" form-control "/>'
+        assert inputs[3] == '<input type="text" name="email" value="new_mail" id="deformField6" class=" form-control "/>'
+
+        from c2cgeoportal_commons.models.main import User
+        user = dbsession.query(User).filter("username='new_name_withéàô'").one_or_none();
+        assert user.email == 'new_mail'
 
     @pytest.mark.usefixtures("raise_db_error_on_query")
     def test_grid_dberror(self, dbsession):
@@ -68,7 +97,7 @@ class TestUser():
     # in order to make this work, had to patch as indicated in https://github.com/SeleniumHQ/selenium/issues/4558
     # and to install selenium gecko driver
     @pytest.mark.usefixtures("selenium", "selenium_app")
-    def test_selenium(self, selenium):
+    def test_selenium(self, dbsession, selenium):
         selenium.get('http://127.0.0.1:6543' + '/user/')
         elem = selenium.find_element_by_xpath("//a[contains(@href,'language=fr')]")
         elem.click()
@@ -78,4 +107,16 @@ class TestUser():
         elem.click()
         elem = selenium.find_element_by_xpath("//a[contains(@href,'13/edit')]")
         elem.click()
-        pass
+        elem = selenium.find_element_by_xpath("//input[@name ='username']")
+        elem.clear()
+        elem.send_keys('new_name_éôù')
+        elem = selenium.find_element_by_xpath("//input[@name ='email']")
+        elem.clear()
+        elem.send_keys('new_email')
+
+        elem = selenium.find_element_by_xpath("//button[@name='formsubmit']")
+        elem.click()
+
+        from c2cgeoportal_commons.models.main import User
+        user = dbsession.query(User).filter("username='new_name_éôù'").one_or_none();
+        assert user.email == 'new_email'
