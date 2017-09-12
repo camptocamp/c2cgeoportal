138896a255c50bdee7f56422fd8379657082c117
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2017-09-12 18:05:50 +0200

    help new/submit driving back to edit
    edit -> view
    edit -> save

--- admin/c2cgeoportal_admin/views/users.py
+++ admin/c2cgeoportal_admin/views/users.py
@@ -40,7 +40,10 @@ class UserViews(AbstractViews):
     def grid(self):
         return super().grid()
 
-    @view_config(route_name='c2cgeoform_action', renderer="c2cgeoform:templates/site/edit.pt")
-    def edit(self):
-        return super().edit()
+    @view_config(route_name='c2cgeoform_action', request_method='GET', renderer="c2cgeoform:templates/site/edit.pt")
+    def view(self):
+        return super().view()
 
+    @view_config(route_name='c2cgeoform_action', request_method='POST', renderer="c2cgeoform:templates/site/edit.pt")
+    def save(self):
+        return super().save()
