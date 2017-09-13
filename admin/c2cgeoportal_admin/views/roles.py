ca8b0ab8f2cca45b7a07a639f487e9e70b073e7a
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2017-09-13 13:55:46 +0200

    activate role edit

--- admin/c2cgeoportal_admin/views/roles.py
+++ admin/c2cgeoportal_admin/views/roles.py
@@ -2,9 +2,22 @@ from pyramid.view import view_defaults
 from pyramid.view import view_config
 from c2cgeoform.views.abstract_views import AbstractViews
 from c2cgeoportal_commons.models.main import Role
+from c2cgeoportal_commons.models.main  import RestrictionArea
 from colanderalchemy import setup_schema
+from c2cgeoform.ext import colander_ext
 
-# setup_schema(None, Role)
+Role.extent.info.update({'colanderalchemy': {
+    'typ': colander_ext.Geometry(
+        'POLYGON', srid=4326, map_srid=3857),
+}})
+
+RestrictionArea.area.info.update({'colanderalchemy': {
+    'typ': colander_ext.Geometry(
+        'POLYGON', srid=4326, map_srid=3857),
+}})
+
+
+setup_schema(None, Role)
 
 @view_config(route_name='role_', renderer='json')
 def role_test_insert(request):
@@ -21,6 +34,7 @@ class UserViews(AbstractViews):
     _list_fields = ['name']
     _id_field = 'id'
     _model = Role
+    _base_schema = Role.__colanderalchemy__
 
     @view_config(route_name='c2cgeoform_index', renderer="c2cgeoform:templates/site/index.pt")
     def index(self):
@@ -30,7 +44,10 @@ class UserViews(AbstractViews):
     def grid(self):
         return super().grid()
 
-    @view_config(route_name='c2cgeoform_action', renderer="c2cgeoform:templates/site/edit.pt")
-    def edit(self):
-        return {}
+    @view_config(route_name='c2cgeoform_action', request_method='GET', renderer="c2cgeoform:templates/site/edit.pt")
+    def view(self):
+        return super().view()
 
+    @view_config(route_name='c2cgeoform_action', request_method='POST', renderer="c2cgeoform:templates/site/edit.pt")
+    def save(self):
+        return super().save()
