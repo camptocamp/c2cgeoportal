45cbb05bbafe20cae0f691f28c30debb20f146d5
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2017-09-13 13:55:46 +0200

    role edition

--- admin/acceptance_tests/user_test.py
+++ admin/acceptance_tests/user_test.py
@@ -1,4 +1,5 @@
 import pytest
+from . import clean_form
 import re
 from pyramid.testing import DummyRequest
 
@@ -6,20 +7,12 @@ from pyramid.testing import DummyRequest
 @pytest.mark.usefixtures("dbsession")
 def insertUsersTestData(dbsession):
     from c2cgeoportal_commons.models.main import User
-    user = User("babar")
     dbsession.begin_nested()
     for i in range (0, 23):
         user = User("babar_" + str(i), email='mail' + str(i))
         dbsession.add(user)
     yield
     dbsession.rollback()
-
-def clean_form(form):
-    clean_form = form.replace('\n', ' ')
-    while re.search('  ', clean_form, re.MULTILINE):
-        clean_form = clean_form.replace('  ', ' ')
-    clean_form = clean_form.replace(' >', '>')
-    return clean_form
 
 @pytest.mark.usefixtures("insertUsersTestData", "transact")
 class TestUser():
@@ -41,6 +34,7 @@ class TestUser():
         assert inputs[0] == '<input type="text" name="username" value="babar_8" id="deformField3" class=" form-control "/>'
         assert inputs[3] == '<input type="text" name="email" value="mail8" id="deformField6" class=" form-control "/>'
 
+    @pytest.mark.usefixtures("test_app")  # route have to be registred for HTTP_FOUND
     def test_submit_update(self, dbsession):
         from c2cgeoportal_admin.views.users import UserViews
         post = {'__formid__': 'deform',
@@ -58,7 +52,7 @@ class TestUser():
         req.matchdict.update({'id': '11'})
         req.matchdict.update({'table': 'user'})
 
-        res = UserViews(req).save()
+        UserViews(req).save()
 
         from c2cgeoportal_commons.models.main import User
         user = dbsession.query(User).filter("username='new_name_withéàô'").one_or_none();
@@ -74,11 +68,13 @@ class TestUser():
     @pytest.mark.usefixtures("test_app")
     def test_view_index_rendering_in_app(self, dbsession, test_app):
         res = test_app.get('/user/', status=200)
-        assert str(res.html.find_all('th', limit=3)) == \
+        res1 = res.click(verbose=True, href='language=en')
+        res2 = res1.follow()
+        assert str(res2.html.find_all('th', limit=3)) == \
              '[<th data-column-id="username">username</th>,' \
              + ' <th data-column-id="email">email</th>,' \
              + ' <th data-column-id="_id_" data-converter="commands" data-searchable="false" data-sortable="false">Commands</th>]'
-        assert len(list(filter(lambda x: str(x.contents) == "['New']", res.html.findAll('a')))) == 1
+        assert len(list(filter(lambda x: str(x.contents) == "['New']", res2.html.findAll('a')))) == 1
 
     @pytest.mark.usefixtures("test_app")
     def test_view_index_rendering_in_app_fr(self, dbsession, test_app):
--- admin/c2cgeoportal_admin/views/roles.py
+++ admin/c2cgeoportal_admin/views/roles.py
@@ -2,15 +2,38 @@ from pyramid.view import view_defaults
 from pyramid.view import view_config
 from c2cgeoform.views.abstract_views import AbstractViews
 from c2cgeoportal_commons.models.main import Role
+from c2cgeoportal_commons.models.main  import RestrictionArea
 from colanderalchemy import setup_schema
+from c2cgeoform.ext import colander_ext, deform_ext
+from deform.widget import HiddenWidget
 
-# setup_schema(None, Role)
+Role.extent.info.update({'colanderalchemy': {
+    'typ': colander_ext.Geometry(
+        'POLYGON', srid=3857, map_srid=3857),
+    'widget': deform_ext.MapWidget()
+
+}})
+
+RestrictionArea.area.info.update({'colanderalchemy': {
+    'typ': colander_ext.Geometry(
+        'POLYGON', srid=3857, map_srid=3857),
+    'widget': deform_ext.MapWidget()
+}})
+
+HIDE_FIELD = {'colanderalchemy': {
+                'widget': HiddenWidget()
+        }}
+
+Role.id.info.update(HIDE_FIELD)
+
+setup_schema(None, Role)
 
 @view_defaults(match_param='table=role')
 class RoleViews(AbstractViews):
     _list_fields = ['name']
     _id_field = 'id'
     _model = Role
+    _base_schema = Role.__colanderalchemy__
 
     @view_config(route_name='c2cgeoform_index', renderer="c2cgeoform:templates/site/index.pt")
     def index(self):
@@ -20,7 +43,10 @@ class RoleViews(AbstractViews):
     def grid(self):
         return super().grid()
 
-    @view_config(route_name='c2cgeoform_action', renderer="c2cgeoform:templates/site/edit.pt")
-    def edit(self):
-        return {}
+    @view_config(route_name='c2cgeoform_action', request_method='GET', renderer="c2cgeoform:templates/site/edit.pt")
+    def view(self):
+        return super().view()
 
+    @view_config(route_name='c2cgeoform_action', request_method='POST', renderer="c2cgeoform:templates/site/edit.pt")
+    def save(self):
+        return super().save()
