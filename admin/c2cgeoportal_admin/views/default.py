386c4d4fcd4919010b1daf4f11cf886128c0c84c
François Van Der Biest <francois.vanderbiest@gmail.com>
2017-09-22 13:59:10 +0200

    remove dummy mymodel

--- admin/c2cgeoportal_admin/views/default.py
+++ admin/c2cgeoportal_admin/views/default.py
@@ -3,17 +3,10 @@ from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
-from ..models import MyModel
-
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
-    try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'c2cgeoportal_admin'}
+    return {'one': 'hello', 'project': 'c2cgeoportal_admin'}
 
 
 db_err_msg = """\
