cc90a04e9e834d7175a4add1f59922b42a78de97
François Van Der Biest <francois.vanderbiest@gmail.com>
2017-09-25 16:09:10 +0200

    flake8 compliance required before image builds

--- admin/c2cgeoportal_admin/__init__.py
+++ admin/c2cgeoportal_admin/__init__.py
@@ -1,4 +1,5 @@
 import c2cwsgiutils.pyramid
+# from c2cgeoportal_commons import models
 from pyramid.config import Configurator
 from c2cwsgiutils.health_check import HealthCheck
 
@@ -7,7 +8,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('c2cwsgiutils.pyramid.includeme')
+    config.include(c2cwsgiutils.pyramid.includeme)
     config.include('pyramid_jinja2')
 
     config.include('c2cgeoportal_commons.models')
@@ -15,8 +16,9 @@ def main(global_config, **settings):
     config.scan()
 
     health_check = HealthCheck(config)
-    #~ health_check.add_db_session_check(models.DBSession, at_least_one_model=models.Hello)
-    #~ health_check.add_url_check('http://localhost/api/hello')
-    #~ health_check.add_alembic_check(models.DBSession, '/app/alembic.ini', 1)
+    # health_check.add_db_session_check(models.DBSession,
+    # at_least_one_model=models.User)
+    health_check.add_url_check('http://localhost/')
+    # health_check.add_alembic_check(models.DBSession, '/app/alembic.ini', 1)
 
     return config.make_wsgi_app()
