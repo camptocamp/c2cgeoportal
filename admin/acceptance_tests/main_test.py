ac85493e85e7669f00ff7db8209987dcc161ece3
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2018-03-07 15:40:58 +0100

    Fix admin dev environment

---
+++ admin/acceptance_tests/main_test.py
@@ -0,0 +1,12 @@
+import pytest
+from pyramid import testing
+from webtest import TestApp as WebTestApp
+
+
+@pytest.mark.usefixtures('app_env')
+def test_main(app_env):
+    """Test dev environment"""
+    config = testing.setUp(registry=app_env['registry'])
+    app = config.make_wsgi_app()
+    testapp = WebTestApp(app)
+    testapp.get('/layertree/nodes', status=200)
