3514ce3a4435917d494a5279bf9050e828e9d563
Stéphane Brunner <stephane.brunner@camptocamp.com>
2018-06-29 10:24:20 +0200

    Better catch permission error

--- geoportal/c2cgeoportal_geoportal/scripts/c2cupgrade.py
+++ geoportal/c2cgeoportal_geoportal/scripts/c2cupgrade.py
@@ -386,7 +386,7 @@ class C2cUpgradeTool:
         print("Files to move")
         error |= self.files_to_move(pre=True)
         print("Files to get")
-        error |= self.files_to_get(pre=True)
+        error |= self.files_to_get(step, pre=True)
 
         if error:
             self.print_step(
@@ -494,7 +494,7 @@ class C2cUpgradeTool:
 
     @Step(7)
     def step7(self, step):
-        self.files_to_get()
+        self.files_to_get(step)
         self.run_step(step + 1)
 
     def is_managed(self, file_):
@@ -541,7 +541,7 @@ class C2cUpgradeTool:
 
         return managed
 
-    def files_to_get(self, pre=False):
+    def files_to_get(self, step, pre=False):
         error = False
         for root, _, files in os.walk("CONST_create_template"):
             root = root[len("CONST_create_template/"):]
@@ -556,8 +556,18 @@ class C2cUpgradeTool:
                     if not pre:
                         if os.path.dirname(destination) != "":
                             os.makedirs(os.path.dirname(destination), exist_ok=True)
-                        shutil.copyfile(source, destination)
-                        shutil.copymode(source, destination)
+                        try:
+                            shutil.copyfile(source, destination)
+                            shutil.copymode(source, destination)
+                        except PermissionError as e:
+                            self.print_step(
+                                step, error=True, message=(
+                                    "All your project files should be own by your user, "
+                                    "current error:\n" + str(e)
+                                ),
+                                prompt="Fix it and run again the upgrade:")
+                            exit(1)
+
                 elif os.path.exists(destination) and not filecmp.cmp(source, destination):
                     print("The file '{}' is managed by the project".format(destination))
                 else:
