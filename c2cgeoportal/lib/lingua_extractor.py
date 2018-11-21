camptocamp/c2cgeoportal

commit 496e2bb23194c23b99c72ed377714fc3610369dc

Centralize the get_layer_class code in a unique place
In addition, this should ensure all created classes for a layer are
consistent.

c2cgeoportal/lib/lingua_extractor.py
@@ -55,7 +55,6 @@ from owslib.wms import WebMapService
 
 from c2cgeoportal.lib import add_url_params, get_url2
 from c2cgeoportal.lib.bashcolor import colorize, RED
-from c2cgeoportal.lib.dbreflection import get_class
 from c2cgeoportal.lib.caching import init_region
 
 
@@ -380,15 +379,9 @@ class GeoMapfishThemeExtractor(Extractor):  # pragma: no cover
         for wms_layer in layer.layer.split(","):
             self._import_layer_attributes(url, wms_layer, layer.item_type, layer.name, messages)
         if layer.geo_table is not None and layer.geo_table != "":
-            exclude = [] if layer.exclude_properties is None else layer.exclude_properties.split(",")
-            last_update_date = layer.get_metadatas("lastUpdateDateColumn")
-            if len(last_update_date) == 1:
-                exclude.append(last_update_date[0].value)
-            last_update_user = layer.get_metadatas("lastUpdateUserColumn")
-            if len(last_update_user) == 1:
-                exclude.append(last_update_user[0].value)
             try:
-                cls = get_class(layer.geo_table, exclude_properties=exclude)
+                from c2cgeoportal.views.layers import get_layer_class
+                cls = get_layer_class(layer)
                 for column_property in class_mapper(cls).iterate_properties:
                     if isinstance(column_property, ColumnProperty) and len(column_property.columns) == 1:
                         column = column_property.columns[0]

c2cgeoportal/views/layers.py
@@ -79,8 +79,7 @@ class Layers:
         This function assumes that the names of geometry attributes
         in the mapped class are the same as those of geometry columns.
         """
-        primary_key = self.get_metadata(layer, "geotablePrimaryKey")
-        mapped_class = get_class(layer.geo_table, primary_key=primary_key)
+        mapped_class = get_layer_class(layer)
         for p in class_mapper(mapped_class).iterate_properties:
             if not isinstance(p, ColumnProperty):
                 continue  # pragma: no cover
@@ -134,7 +133,7 @@ class Layers:
 
     def _get_protocol_for_layer(self, layer, **kwargs):
         """ Returns a papyrus ``Protocol`` for the ``Layer`` object. """
-        cls = get_class(layer.geo_table)
+        cls = get_layer_class(layer)
         geom_attr = self._get_geom_col_info(layer)[0]
         return Protocol(DBSession, cls, geom_attr, **kwargs)
 
@@ -415,22 +414,7 @@ class Layers:
         layer = self._get_layer_for_request()
         if not layer.public and self.request.user is None:
             raise HTTPForbidden()
-
-        # exclude the columns used to record the last features update
-        exclude = [] if layer.exclude_properties is None else layer.exclude_properties.split(",")
-        last_update_date = self.get_metadata(layer, "lastUpdateDateColumn")
-        if last_update_date is not None:
-            exclude.append(last_update_date)
-        last_update_user = self.get_metadata(layer, "lastUpdateUserColumn")
-        if last_update_user is not None:
-            exclude.append(last_update_user)
-
-        primary_key = self.get_metadata(layer, "geotablePrimaryKey")
-        return get_class(
-            str(layer.geo_table),
-            exclude_properties=exclude,
-            primary_key=primary_key
-        )
+        return get_layer_class(layer)
 
     @view_config(route_name="layers_enumerate_attribute_values", renderer="json")
     def enumerate_attribute_values(self):
@@ -484,26 +468,26 @@ class Layers:
         return dbsession.query(distinct(attribute)).order_by(attribute).all()
 
 
-def get_layer_metadatas(layer):
+def get_layer_class(layer):
     # exclude the columns used to record the last features update
     exclude = [] if layer.exclude_properties is None else layer.exclude_properties.split(",")
-
-    date_metadata = layer.get_metadatas("lastUpdateDateColumn")
-    last_update_date = date_metadata[0] if len(date_metadata) == 1 else None
+    last_update_date = Layers.get_metadata(layer, "lastUpdateDateColumn")
     if last_update_date is not None:
-        exclude.append(last_update_date.value)
-    user_metadata = layer.get_metadatas("lastUpdateUserColumn")
-    last_update_user = user_metadata[0] if len(user_metadata) == 1 else None
+        exclude.append(last_update_date)
+    last_update_user = Layers.get_metadata(layer, "lastUpdateUserColumn")
     if last_update_user is not None:
-        exclude.append(last_update_user.value)
+        exclude.append(last_update_user)
 
     primary_key = Layers.get_metadata(layer, "geotablePrimaryKey")
-    cls = get_class(
+    return get_class(
         str(layer.geo_table),
         exclude_properties=exclude,
         primary_key=primary_key
     )
 
+
+def get_layer_metadatas(layer):
+    cls = get_layer_class(layer)
     edit_columns = []
 
     for column_property in class_mapper(cls).iterate_properties:

commit c9b37b3f291712e9f2f032c3439f74ee0e1b3f08

Don't exclude properties everywhere
Fix regression introduced by
https://github.com/camptocamp/c2cgeoportal/commit/496e2bb23194c23b99c72ed377714fc3610369dc

c2cgeoportal/lib/lingua_extractor.py
@@ -377,7 +377,7 @@ class GeoMapfishThemeExtractor(Extractor):  # pragma: no cover
         if layer.geo_table is not None and layer.geo_table != "":
             try:
                 from c2cgeoportal.views.layers import get_layer_class
-                cls = get_layer_class(layer)
+                cls = get_layer_class(layer, with_exclude=True)
                 for column_property in class_mapper(cls).iterate_properties:
                     if isinstance(column_property, ColumnProperty) and len(column_property.columns) == 1:
                         column = column_property.columns[0]

c2cgeoportal/views/layers.py
@@ -414,7 +414,7 @@ class Layers:
         layer = self._get_layer_for_request()
         if not layer.public and self.request.user is None:
             raise HTTPForbidden()
-        return get_layer_class(layer)
+        return get_layer_class(layer, with_exclude=True)
 
     @view_config(route_name="layers_enumerate_attribute_values", renderer="json")
     def enumerate_attribute_values(self):
@@ -468,15 +468,18 @@ class Layers:
         return dbsession.query(distinct(attribute)).order_by(attribute).all()
 
 
-def get_layer_class(layer):
-    # exclude the columns used to record the last features update
-    exclude = [] if layer.exclude_properties is None else layer.exclude_properties.split(",")
-    last_update_date = Layers.get_metadata(layer, "lastUpdateDateColumn")
-    if last_update_date is not None:
-        exclude.append(last_update_date)
-    last_update_user = Layers.get_metadata(layer, "lastUpdateUserColumn")
-    if last_update_user is not None:
-        exclude.append(last_update_user)
+def get_layer_class(layer, with_exclude=False):
+    if with_exclude:
+        # Exclude the columns used to record the last features update
+        exclude = [] if layer.exclude_properties is None else layer.exclude_properties.split(",")
+        last_update_date = Layers.get_metadata(layer, "lastUpdateDateColumn")
+        if last_update_date is not None:
+            exclude.append(last_update_date)
+        last_update_user = Layers.get_metadata(layer, "lastUpdateUserColumn")
+        if last_update_user is not None:
+            exclude.append(last_update_user)
+    else:
+        exclude = []
 
     primary_key = Layers.get_metadata(layer, "geotablePrimaryKey")
     return get_class(
@@ -487,7 +490,7 @@ def get_layer_class(layer):
 
 
 def get_layer_metadatas(layer):
-    cls = get_layer_class(layer)
+    cls = get_layer_class(layer, with_exclude=True)
     edit_columns = []
 
     for column_property in class_mapper(cls).iterate_properties: