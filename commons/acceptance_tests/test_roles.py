3732145f6cffc2c7fe93d4f9af061e62edc23e5f
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2018-12-12 12:56:02 +0100

    [multi_role] Cascade delete role to user_role using backref

--- commons/acceptance_tests/test_roles.py
+++ commons/acceptance_tests/test_roles.py
@@ -5,9 +5,13 @@ import pytest
 @pytest.mark.usefixtures("dbsession")
 def insert_roles_test_data(dbsession):
     from c2cgeoportal_commons.models.main import Role
+    from c2cgeoportal_commons.models.static import User
     role = Role("secretary")
+    user = User("user1", roles=[role])
     t = dbsession.begin_nested()
     dbsession.add(role)
+    dbsession.add(user)
+    dbsession.flush()
     yield
     t.rollback()
 
@@ -20,7 +24,12 @@ class TestRole:
         assert len(roles) == 1, "querying for roles"
         assert roles[0].name == 'secretary', "role from test data is secretary"
 
-    def test_no_user(self, dbsession):
-        from c2cgeoportal_commons.models.static import User
-        users = dbsession.query(User).all()
-        assert len(users) == 0, "querying for users"
+    def test_delete(self, dbsession):
+        from c2cgeoportal_commons.models.main import Role
+        from c2cgeoportal_commons.models.static import user_role
+        roles = dbsession.query(Role).all()
+        dbsession.delete(roles[0])
+        roles = dbsession.query(Role).all()
+        assert len(roles) == 0, "removed a role"
+        assert 0 == dbsession.query(user_role).\
+            count()
