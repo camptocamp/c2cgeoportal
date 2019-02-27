af978cd601f4720dbbf09e41b2f649943a25253f
Frederic Junod <frederic.junod@camptocamp.com>
2019-02-27 13:33:41 +0100

    Api integration

---
+++ geoportal/c2cgeoportal_geoportal/scaffolds/create/geoportal/+package+_geoportal/static-ngeo/api/index.js
@@ -0,0 +1,10 @@
+import Map from 'api/Map.js';
+import * as constants from 'api/constants.js';
+
+constants.themesUrl = 'https://www.example.com';
+
+const lib = {
+  Map
+};
+
+export default lib;
--- geoportal/c2cgeoportal_geoportal/scripts/c2cupgrade.py
+++ geoportal/c2cgeoportal_geoportal/scripts/c2cupgrade.py
@@ -514,7 +514,7 @@ class C2cUpgradeTool:
         # managed means managed by the application owner, not the c2cupgrade
         managed = False
         if not files_to_get or os.path.exists(file_) or not \
-                check_output(["git", "status", "--short", file_]).decode("utf-8").startswith("?? "):
+                check_output(["git", "status", "--short", "CONST_create_template/" + file_]).decode("utf-8").startswith("?? "):
             for pattern in default_project_file['include']:
                 if re.match(pattern + '$', file_):
                     print("File '{}' included by migration config pattern '{}'.".format(file_, pattern))
