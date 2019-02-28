eaea7dde641bdb30f300327810bbe117bc3e2cce
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-02-28 16:41:50 +0100

    Don't pull unwanted images

--- geoportal/c2cgeoportal_geoportal/scripts/c2cupgrade.py
+++ geoportal/c2cgeoportal_geoportal/scripts/c2cupgrade.py
@@ -705,7 +705,7 @@ class C2cUpgradeTool:
                 "The upgrade is nearly done, now you should:",
                 "- To upgrade the database run `./docker-compose-run alembic --name=main "
                 "--config=geoportal/alembic.ini upgrade head`",
-                "- Run `docker-compose pull --ignore-pull-failures && "
+                "- Run `DOCKER_TAG=unexisting docker-compose pull --ignore-pull-failures && "
                 "docker-compose down --remove-orphans && docker-compose up -d`.",
                 "- Test your application on '{}'.".format(
                     self.project.get('application_url', '... missing ...')
