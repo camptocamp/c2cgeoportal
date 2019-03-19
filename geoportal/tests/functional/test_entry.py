089b5fa467696d459144b84211668b8f94fd595e
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-03-19 13:51:21 +0100

    Fix tests

--- geoportal/tests/functional/test_entry.py
+++ geoportal/tests/functional/test_entry.py
@@ -214,7 +214,7 @@ class TestEntryView(TestCase):
     #
 
     def test_login(self):
-        from pyramid.httpexceptions import HTTPBadRequest
+        from pyramid.httpexceptions import HTTPForbidden
         from c2cgeoportal_geoportal.views.entry import Entry
 
         request = self._create_request_obj(params={
@@ -247,7 +247,7 @@ class TestEntryView(TestCase):
             "password": "bad password",
         })
         entry = Entry(request)
-        self.assertRaises(HTTPBadRequest, entry.login)
+        self.assertRaises(HTTPForbidden, entry.login)
 
     def test_logout_no_auth(self):
         from pyramid.httpexceptions import HTTPBadRequest
