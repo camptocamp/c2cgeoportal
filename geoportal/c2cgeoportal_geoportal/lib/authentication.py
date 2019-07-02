f00690f001b1ea6b67591095570ea3e4bf192b8d
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-07-02 17:01:27 +0200

    Fix typo

--- geoportal/c2cgeoportal_geoportal/lib/authentication.py
+++ geoportal/c2cgeoportal_geoportal/lib/authentication.py
@@ -57,7 +57,7 @@ def create_authentication(settings):
         secret,
         callback=defaultgroupsfinder,
         cookie_name=settings["authtkt_cookie_name"],
-        samesite=samesite is None if samesite == '' else samesite,
+        samesite=None if samesite == '' else samesite,
         timeout=timeout, max_age=timeout, reissue_time=reissue_time,
         hashalg="sha512", http_only=http_only, secure=secure,
     )
