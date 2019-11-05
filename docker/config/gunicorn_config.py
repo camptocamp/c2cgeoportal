d0b49b5b3b8f58a61a6c349c24166e40aeff8364
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-11-05 17:46:55 +0100

    Fix Gunicorn access log format

--- docker/config/gunicorn_config.py
+++ docker/config/gunicorn_config.py
@@ -1,2 +1,2 @@
 accesslog = "-"
-access_log_format = '%(r)s ----- %(H)s %({Host}i)s %(m)s %(U)s %(q)s "%(f)s" "%(a)s" %(s)s %(B)s %(D)s %(p)s'
+access_log_format = '%(H)s %({Host}i)s %(m)s %(U)s %(q)s "%(f)s" "%(a)s" %(s)s %(B)s %(D)s %(p)s'
