4aef7aa3a99e16396d5c530e42268071cd8d579c
Stéphane Brunner <stephane.brunner@camptocamp.com>
2021-04-09 15:01:29 +0200

    Consolidate the TileCloud-chain documentation

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,11 @@ html_theme_path = [alabaster.get_path()]
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.todo", "sphinx-prompt", "alabaster"]
+extensions = [
+    "sphinx-prompt",
+    "alabaster",
+    "sphinx-jsonschema",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
