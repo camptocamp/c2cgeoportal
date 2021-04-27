2db11265468322600c07e64af566d6843d55e438
Stéphane Brunner <stephane.brunner@camptocamp.com>
2021-04-27 17:10:11 +0200

    Add possibility to have custom interface name per organisation instance
    And rename organization to organisation

--- geoportal/c2cgeoportal_geoportal/views/dynamic.py
+++ geoportal/c2cgeoportal_geoportal/views/dynamic.py
@@ -101,7 +101,7 @@ class DynamicView:
 
     @view_config(route_name="dynamic", renderer="json")  # type: ignore
     def dynamic(self) -> Dict[str, Any]:
-        interface_name = self.request.params.get("interface")
+        interface_name = self.request.get_organization_interface(self.request.params.get("interface"))
 
         if interface_name not in self.interfaces_config:
             raise HTTPNotFound("Interface {} doesn't exists in the 'interfaces_config'.")
