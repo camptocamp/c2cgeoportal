8b8ed058fc158bab1d2d91c83185d1208b26dca3
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2021-05-10 19:42:26 +0200

    Add test on boolean metadata rendering

--- admin/tests/test_metadatas.py
+++ admin/tests/test_metadatas.py
@@ -77,6 +77,15 @@ class TestMetadatasView(AbstractViewsTests):
             "string",
         )
 
+    def expected_value(self, test_app, metadata):
+        if self.__metadata_ui_type(test_app, metadata.name) == "boolean":
+            if metadata.value == "true":
+                return True
+            if metadata.value == "false":
+                return False
+            return None
+        return metadata.value
+
     def _check_metadatas(self, test_app, item, metadatas):
         settings = test_app.app.registry.settings
         self._check_sequence(
@@ -94,7 +103,10 @@ class TestMetadatasView(AbstractViewsTests):
                         ],
                         "label": "Name",
                     },
-                    {"name": self.__metadata_ui_type(test_app, m.name), "value": m.value},
+                    {
+                        "name": self.__metadata_ui_type(test_app, m.name),
+                        "value": self.expected_value(test_app, m),
+                    },
                     {"name": "description", "value": m.description, "label": "Description"},
                 ]
                 for m in metadatas
@@ -143,6 +155,42 @@ class TestMetadatasView(AbstractViewsTests):
             "number",
             '"number" is not a number',
         )
+
+    def test_get_true_boolean_metadata(self, metadatas_test_data, test_app):
+        metadatas_test_data["layer_wms"].get_metadatas("_boolean")[0].value = "true"
+        self._test_edit_treeitem("layers_wms", metadatas_test_data["layer_wms"], test_app)
+
+    def test_get_false_boolean_metadata(self, metadatas_test_data, test_app):
+        metadatas_test_data["layer_wms"].get_metadatas("_boolean")[0].value = "false"
+        self._test_edit_treeitem("layers_wms", metadatas_test_data["layer_wms"], test_app)
+
+    def test_post_true_boolean_metadata(self, test_app, metadatas_test_data, dbsession):
+        from c2cgeoportal_commons.models.main import LayerWMS
+
+        self._post_metadata(
+            test_app,
+            "/admin/layers_wms/new",
+            self._base_metadata_params(metadatas_test_data),
+            "_boolean",
+            "true",
+            302,
+        )
+        layer = dbsession.query(LayerWMS).filter(LayerWMS.name == "new_name").one()
+        assert layer.get_metadatas("_boolean")[0].value == "true"
+
+    def test_post_false_boolean_metadata(self, test_app, metadatas_test_data, dbsession):
+        from c2cgeoportal_commons.models.main import LayerWMS
+
+        self._post_metadata(
+            test_app,
+            "/admin/layers_wms/new",
+            self._base_metadata_params(metadatas_test_data),
+            "_boolean",
+            "false",
+            302,
+        )
+        layer = dbsession.query(LayerWMS).filter(LayerWMS.name == "new_name").one()
+        assert layer.get_metadatas("_boolean")[0].value == "false"
 
     def test_valid_float_metadata(self, test_app, metadatas_test_data):
         self._post_metadata(
