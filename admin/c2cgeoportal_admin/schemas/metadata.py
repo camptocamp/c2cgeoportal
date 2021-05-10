081f326bfe0d40516d2534da054e2e667897ec1d
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2021-05-10 19:42:26 +0200

    Add test on boolean metadata rendering

--- admin/c2cgeoportal_admin/schemas/metadata.py
+++ admin/c2cgeoportal_admin/schemas/metadata.py
@@ -96,13 +96,25 @@ def regex_validator(node, value):
             raise error
 
 
-class CustomBoolean(colander.Boolean):
+class BooleanMetadata(colander.Boolean):  # type: ignore
+    """Boolean metadata values are stored as string in database"""
+
     def serialize(self, node, appstruct):
-        if appstruct is colander.null:
-            return colander.null
         if appstruct == "true":
-            return self.true_val
-        return self.false_val
+            appstruct = True
+        elif appstruct == "false":
+            appstruct = False
+        else:
+            appstruct = colander.null
+        return super().serialize(node, appstruct)
+
+    def deserialize(self, node, cstruct):
+        appstruct = super().deserialize(node, cstruct)
+        if appstruct is True:
+            return "true"
+        if appstruct is False:
+            return "false"
+        return None
 
 
 class MetadataSchemaNode(GeoFormSchemaNode):  # type: ignore # pylint: disable=abstract-method
@@ -116,7 +128,7 @@ class MetadataSchemaNode(GeoFormSchemaNode):  # type: ignore # pylint: disable=a
 
         self._add_value_node("string", colander.String())
         self._add_value_node("liste", colander.String())
-        self._add_value_node("boolean", CustomBoolean())
+        self._add_value_node("boolean", BooleanMetadata())
         self._add_value_node("int", colander.Int())
         self._add_value_node("float", colander.Float())
         self._add_value_node("url", colander.String(), validator=url)
