f781ae755c9bfe2f096b8271aeee77375dee660d
Benjamin <benjamin.gerber@camptocamp.com>
2021-09-08 18:56:04 +0200

    Add blacklist whitelist for headers (#8650)
    * Add blacklist whitelist for headers
    
    * Fix failing test
    
    * Functional syntax

--- geoportal/c2cgeoportal_geoportal/views/proxy.py
+++ geoportal/c2cgeoportal_geoportal/views/proxy.py
@@ -52,7 +52,9 @@ CACHE_REGION = get_region("std")
 class Proxy:
     def __init__(self, request):
         self.request = request
-        self.host_forward_host = request.registry.settings["host_forward_host"]
+        self.host_forward_host = request.registry.settings.get("host_forward_host", [])
+        self.headers_whitelist = request.registry.settings.get("headers_whitelist", [])
+        self.headers_blacklist = request.registry.settings.get("headers_blacklist", [])
         self.http_options = self.request.registry.settings.get("http_options", {})
 
     def _proxy(self, url, params=None, method=None, cache=False, body=None, headers=None):
@@ -80,7 +82,8 @@ class Proxy:
         if headers is None:  # pragma: no cover
             headers = dict(self.request.headers)
 
-        # Forward request to target (without Host Header)
+        # Forward request to target (without Host Header).
+        # The original Host will be added back by pyramid.
         if parsed_url.hostname not in self.host_forward_host and "Host" in headers:  # pragma: no cover
             headers.pop("Host")
 
@@ -104,6 +107,15 @@ class Proxy:
 
         if method in ("POST", "PUT") and body is None:  # pragma: no cover
             body = self.request.body
+
+        # Filters headers with a whitelist.
+        # Some default pyramid headers will be added back by pyramid.
+        if len(self.headers_whitelist) > 0:
+            headers = {key: value for key, value in headers.items() if key in self.headers_whitelist}
+
+        # Filters headers with a blacklist.
+        # Some default pyramid headers will be added back by pyramid.
+        headers = {key: value for key, value in headers.items() if key not in self.headers_blacklist}
 
         try:
             if method in ("POST", "PUT"):
