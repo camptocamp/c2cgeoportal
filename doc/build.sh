c01e7a3e1e61d98be1cb55a97f48808c46d12e4c
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-06-14 17:57:23 +0200

    Run prettier, fix it

--- doc/build.sh
+++ doc/build.sh
@@ -2,13 +2,12 @@
 
 cd $(dirname ${0})
 exec 3>&1
-error=$( { sphinx-build -b html -d _build/doctrees . _build/html 1>&3; } 2>&1 )
+error=$({ sphinx-build -b html -d _build/doctrees . _build/html 1>&3; } 2>&1)
 exec 3>&-
 cd - > /dev/null
-if [ "${error}" != "" ];
-then
-  echo Documentation Error:
-  echo ${error}
-  exit 1
+if [ "${error}" != "" ]; then
+    echo Documentation Error:
+    echo ${error}
+    exit 1
 fi
 echo Documentation Success
