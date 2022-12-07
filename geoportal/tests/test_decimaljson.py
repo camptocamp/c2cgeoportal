bfac9cca872bce8506da904294a3d920e8b29e5b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-12-07 16:40:27 +0100

    Update to test the new version of fast dump

--- geoportal/tests/test_decimaljson.py
+++ geoportal/tests/test_decimaljson.py
@@ -32,14 +32,16 @@ import decimal
 import json
 from unittest import TestCase
 
-from c2cwsgiutils.pretty_json import fast_dumps
+from c2cwsgiutils.pretty_json import _FastDumps as FastDumps
 
 
 class TestDecimalJSON(TestCase):
     def test_decimal(self):
         value = {"str": "an str", "int": 1, "dec": decimal.Decimal("1.2")}
+        fast_dumps = FastDumps(False, False)
         result = fast_dumps(value)
         self.assertEqual(json.loads(result), {"int": 1, "dec": 1.2, "str": "an str"})
 
     def test_decimal_json(self):
-        assert fast_dumps({"a": decimal.Decimal("3.3")}) == '{\n  "a": 3.3\n}'
+        fast_dumps = FastDumps(False, False)
+        assert fast_dumps({"a": decimal.Decimal("3.3")}) == '{"a":3.3}'
