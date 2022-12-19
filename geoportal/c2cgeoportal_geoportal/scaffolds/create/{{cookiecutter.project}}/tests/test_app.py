450ac686e87aad93425caceeccbb6897e875a342
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-12-19 17:36:39 +0100

    Update the timeout

--- geoportal/c2cgeoportal_geoportal/scaffolds/create/{{cookiecutter.project}}/tests/test_app.py
+++ geoportal/c2cgeoportal_geoportal/scaffolds/create/{{cookiecutter.project}}/tests/test_app.py
@@ -6,33 +6,29 @@ import requests
 
 
 @pytest.mark.parametrize(
-    "url,params",
+    "url,params,timeout",
     [
-        ("https://front", {}),
-        ("https://front/themes", {}),
-        ("https://front/static-geomapfish/0/locales/fr.json", {}),
-        ("https://front/dynamic.json", {"interface": "desktop"}),
-        ("https://front/dynamic.json", {"interface": "desktop", "query": "", "path": "/"}),
-        ("https://front/c2c/health_check", {}),
-        ("https://front/c2c/health_check", {"max_level": "1"}),
-        ("https://front/c2c/health_check", {"checker": "check_collector"}),
-        ("https://front/admin/layertree", {}),
-        ("https://front/admin/layertree/children", {}),
-        ("http://mapserver:8080/mapserv_proxy", {"SERVICE": "WMS", "REQUEST": "GetCapabilities"}),
+        ("https://front", {}, 10),
+        ("https://front/themes", {}, 120),
+        ("https://front/static-geomapfish/0/locales/fr.json", {}, 2),
+        ("https://front/dynamic.json", {"interface": "desktop"}, 10),
+        ("https://front/dynamic.json", {"interface": "desktop", "query": "", "path": "/"}, 10),
+        ("https://front/c2c/health_check", {}, 2),
+        ("https://front/c2c/health_check", {"max_level": "1"}, 2),
+        ("https://front/c2c/health_check", {"checker": "check_collector"}, 2),
+        ("https://front/admin/layertree", {}, 10),
+        ("https://front/admin/layertree/children", {}, 10),
+        ("http://mapserver:8080/mapserv_proxy", {"SERVICE": "WMS", "REQUEST": "GetCapabilities"}, 60),
         (
             "https://front/mapserv_proxy",
             {"ogcserver": "source for image/png", "SERVICE": "WMS", "REQUEST": "GetCapabilities"},
+            60,
         ),
     ],
 )
-def test_url(url: str, params: Dict[str, str]) -> None:
+def test_url(url: str, params: Dict[str, str], timeout: int) -> None:
     """Tests that some URL didn't return an error."""
-    for _ in range(6):
-        response = requests.get(url, params=params, verify=False, timeout=10)  # nosec
-        if response.status_code == 503:
-            time.sleep(1)
-            continue
-        break
+    response = requests.get(url, params=params, verify=False, timeout=timeout)  # nosec
     assert response.status_code == 200, response.text
 
 
