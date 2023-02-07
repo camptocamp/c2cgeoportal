76ac62565656e454c0512f534292f84cbabacc08
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-02-07 11:36:43 +0100

    Fix the copyright

--- commons/c2cgeoportal_commons/models/sqlalchemy.py
+++ commons/c2cgeoportal_commons/models/sqlalchemy.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2012-2020, Camptocamp SA
+# Copyright (c) 2012-2023, Camptocamp SA
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
