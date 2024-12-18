31a32faf53de0df8781cb850d95397938bc96017
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-12-18 17:18:44 +0100

    Don't have an authenticatication loop
    When we want to access to the admin interface with oidc and with a user
    that's not allowed to access to the admin interface.

--- geoportal/c2cgeoportal_geoportal/views/login.py
+++ geoportal/c2cgeoportal_geoportal/views/login.py
@@ -93,6 +93,9 @@ class Login:
 
     @forbidden_view_config(renderer="login.html")  # type: ignore[misc]
     def loginform403(self) -> dict[str, Any] | pyramid.response.Response:
+        if self.request.authenticated_userid is not None:
+            return HTTPForbidden()
+
         if self.authentication_settings.get("openid_connect", {}).get("enabled", False):
             return HTTPFound(
                 location=self.request.route_url(
@@ -100,9 +103,6 @@ class Login:
                     _query={"came_from": f"{self.request.path}?{urllib.parse.urlencode(self.request.GET)}"},
                 )
             )
-
-        if self.request.authenticated_userid is not None:
-            return HTTPForbidden()
 
         set_common_headers(self.request, "login", Cache.PRIVATE_NO)
 
