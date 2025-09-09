9a19ee3535651ddf2ac8c243c5668fe1e9f05657
Stéphane Brunner <stephane.brunner@camptocamp.com>
2025-09-09 16:08:29 +0200

    Ignore PyLint issue in project

--- geoportal/c2cgeoportal_geoportal/scaffolds/advance_create/{{cookiecutter.project}}/geoportal/{{cookiecutter.package}}_geoportal/models.py
+++ geoportal/c2cgeoportal_geoportal/scaffolds/advance_create/{{cookiecutter.project}}/geoportal/{{cookiecutter.package}}_geoportal/models.py
@@ -1,6 +1,6 @@
 import logging
 
-from c2cgeoportal_commons.models.main import *
+from c2cgeoportal_commons.models.main import *  # pylint: disable=unused-wildcard-import
 from pyramid.i18n import TranslationStringFactory
 
 _ = TranslationStringFactory("{{cookiecutter.package}}_geoportal-server")
