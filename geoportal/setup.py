0aacac899160339b9f9663eb61f86bda597856bf
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-01-29 14:53:03 +0100

    Update the mainly used Python version

--- geoportal/setup.py
+++ geoportal/setup.py
@@ -63,7 +63,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: GIS",
         "Typing :: Typed",
     ],
