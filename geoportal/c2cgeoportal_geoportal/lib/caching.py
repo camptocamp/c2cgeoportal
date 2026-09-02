e59e8bd764708feb5c6796d0e9301ddcaec58482
Guillaume REMY <guillaume.remy@bs.ch>
2026-09-02 14:26:00 +0200

    Fix error when deleting entry in the cache

--- geoportal/c2cgeoportal_geoportal/lib/caching.py
+++ geoportal/c2cgeoportal_geoportal/lib/caching.py
@@ -160,11 +160,11 @@ class HybridRedisBackend(CacheBackend):
 
     def delete(self, key: str) -> None:
         self._memory.delete(key)
-        self._redis.delete(key)
+        self._redis.delete(sha1_mangle_key(key.encode()))
 
     def delete_multi(self, keys: Iterable[str]) -> None:
-        self._memory.delete_multi(keys)
-        self._redis.delete_multi(keys)
+        for key in keys:
+            self.delete(key)
 
 
 class HybridRedisSentinelBackend(HybridRedisBackend):
